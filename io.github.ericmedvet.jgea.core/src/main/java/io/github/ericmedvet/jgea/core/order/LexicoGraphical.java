29c7f17e4e9596b3a09aa794b8f138fbdd9b0bf9
Eric Medvet <eric.medvet@gmail.com>
2025-09-03 11:01:52 +0200

    fix: make `LexicoGraphical` a total instead of partial order

--- io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/order/LexicoGraphical.java
+++ io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/order/LexicoGraphical.java
@@ -20,10 +20,10 @@
 
 package io.github.ericmedvet.jgea.core.order;
 
+import java.util.Comparator;
 import java.util.List;
 
-// TODO this should be a Comparator
-public class LexicoGraphical<C extends Comparable<C>> implements PartialComparator<List<C>> {
+public class LexicoGraphical<C extends Comparable<C>> implements Comparator<List<C>> {
 
   private final int[] order;
 
@@ -32,21 +32,15 @@ public class LexicoGraphical<C extends Comparable<C>> implements PartialComparat
   }
 
   @Override
-  public PartialComparatorOutcome compare(List<C> k1, List<C> k2) {
-    if (k1.size() != k2.size()) {
-      return PartialComparatorOutcome.NOT_COMPARABLE;
-    }
+  public int compare(List<C> k1, List<C> k2) {
     for (int i : order) {
       C o1 = k1.get(i);
       C o2 = k2.get(i);
       int outcome = o1.compareTo(o2);
-      if (outcome < 0) {
-        return PartialComparatorOutcome.BEFORE;
-      }
-      if (outcome > 0) {
-        return PartialComparatorOutcome.AFTER;
+      if (outcome != 0) {
+        return outcome;
       }
     }
-    return PartialComparatorOutcome.SAME;
+    return 0;
   }
 }
